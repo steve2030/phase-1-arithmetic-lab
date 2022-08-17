52bc051604223eed8b6a9387e219e63c414518da
steve <stevemighty2030@gmail.com>
2022-08-17 23:20:53 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+
+const multiply = num1 * num2;
+
+// Random Number
+let random = Math.floor(Math.random() * 100) + 1;
+
+// geting the mode value
+let num3 = 22;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+// Get the highest value in a set
+
+let max = Math.max(8, 4, 20, 7, 9, 5, 19);
